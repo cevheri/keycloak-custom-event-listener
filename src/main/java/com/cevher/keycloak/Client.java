b38b27a18a7f9060900a5a27e2dd72b963d832c2
Cevheri <cevheribozoglan@gmail.com>
2021-09-10 08:46:27 +0300

    first commit

---
+++ src/main/java/com/cevher/keycloak/Client.java
@@ -0,0 +1,58 @@
+package com.cevher.keycloak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Client {
+
+
+    public static void postService(String data) {
+        System.out.println("Hi, its Client class");
+        try {
+
+            URL url = new URL("https://613933731fcce10017e78a68.mockapi.io/api/v1/users");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+
+            String input = data;
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/cevher/keycloak/CustomEventListenerProvider.java
@@ -0,0 +1,158 @@
+package com.cevher.keycloak;
+
+import org.jboss.logging.Logger;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.events.admin.ResourceType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.RealmProvider;
+import org.keycloak.models.UserModel;
+
+import java.util.Map;
+
+public class CustomEventListenerProvider
+        implements EventListenerProvider {
+
+    private static final Logger log = Logger.getLogger(CustomEventListenerProvider.class);
+
+    private final KeycloakSession session;
+    private final RealmProvider model;
+
+    public CustomEventListenerProvider(KeycloakSession session) {
+        this.session = session;
+        this.model = session.realms();
+    }
+
+    @Override
+    public void onEvent(Event event) {
+
+        log.debugf("New %s Event", event.getType());
+        log.debugf("onEvent-> %s", toString(event));
+
+        if (EventType.REGISTER.equals(event.getType())) {
+
+            event.getDetails().forEach((key, value) -> log.debugf("%s : %s",key, value));
+
+            RealmModel realm = this.model.getRealm(event.getRealmId());
+            UserModel user = this.session.users().getUserById(event.getUserId(), realm);
+            sendUserData(user);
+        }
+
+    }
+
+
+
+    @Override
+    public void onEvent(AdminEvent adminEvent, boolean b) {
+        log.debug("onEvent(AdminEvent)");
+        log.debugf("Resource path: %s", adminEvent.getResourcePath());
+        log.debugf("Resource type: %s", adminEvent.getResourceType());
+        log.debugf("Operation type: %s", adminEvent.getOperationType());
+        log.debugf("AdminEvent.toString(): %s", toString(adminEvent));
+        if (ResourceType.USER.equals(adminEvent.getResourceType())
+                && OperationType.CREATE.equals(adminEvent.getOperationType())) {
+            RealmModel realm = this.model.getRealm(adminEvent.getRealmId());
+            UserModel user = this.session.users().getUserById(adminEvent.getResourcePath().substring(6), realm);
+
+            sendUserData(user);
+        }
+    }
+    private void sendUserData(UserModel user) {
+        String data =
+                "{\"id\": " + user.getId() + "\"," +
+                        "{\"email\": " + user.getEmail() + "\"," +
+                        "\"userName\":\"" + user.getUsername() + "\"," +
+                        "\"firstName\":\"" + user.getFirstName() + "\"," +
+                        "\"lastName\":\"" + user.getLastName() + "\"," +
+                        "}";
+        try {
+            Client.postService(data);
+            log.debug("A new user has been created and post API");
+        } catch (Exception e) {
+            log.errorf("Failed to call API: %s", e);
+        }
+    }
+    @Override
+    public void close() {}
+
+    private String toString(Event event) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("type=");
+        sb.append(event.getType());
+        sb.append(", realmId=");
+        sb.append(event.getRealmId());
+        sb.append(", clientId=");
+        sb.append(event.getClientId());
+        sb.append(", userId=");
+        sb.append(event.getUserId());
+        sb.append(", ipAddress=");
+        sb.append(event.getIpAddress());
+        if (event.getError() != null) {
+            sb.append(", error=");
+            sb.append(event.getError());
+        }
+
+
+        if (event.getDetails() != null) {
+            for (Map.Entry<String, String> e : event.getDetails().entrySet()) {
+                sb.append(", ");
+                sb.append(e.getKey());
+                if (e.getValue() == null || e.getValue().indexOf(' ') == -1) {
+                    sb.append("=");
+                    sb.append(e.getValue());
+                } else {
+                    sb.append("='");
+                    sb.append(e.getValue());
+                    sb.append("'");
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String toString(AdminEvent event) {
+
+        RealmModel realm = this.model.getRealm(event.getRealmId());
+
+        UserModel newRegisteredUser =
+                this.session.users().getUserById(event.getAuthDetails().getUserId(), realm);
+
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("operationType=");
+        sb.append(event.getOperationType());
+        sb.append(", realmId=");
+        sb.append(event.getAuthDetails().getRealmId());
+        sb.append(", clientId=");
+        sb.append(event.getAuthDetails().getClientId());
+        sb.append(", userId=");
+        sb.append(event.getAuthDetails().getUserId());
+
+        if (newRegisteredUser != null) {
+            sb.append(", email=");
+            sb.append(newRegisteredUser.getEmail());
+            sb.append(", getUsername=");
+            sb.append(newRegisteredUser.getUsername());
+            sb.append(", getFirstName=");
+            sb.append(newRegisteredUser.getFirstName());
+            sb.append(", getLastName=");
+            sb.append(newRegisteredUser.getLastName());
+        }
+        sb.append(", ipAddress=");
+        sb.append(event.getAuthDetails().getIpAddress());
+        sb.append(", resourcePath=");
+        sb.append(event.getResourcePath());
+        if (event.getError() != null) {
+            sb.append(", error=");
+            sb.append(event.getError());
+        }
+
+        return sb.toString();
+    }
+}
